d4a09708824b00f04e1e9c0c5c5f821d4b9e52d4
pankajigec26 <pankajigec26@gmail.com>
2019-06-21 18:31:32 +0530

    to be used with python2.7
    Pass movie name as an argument  i.e. python2.7 Imdb.py "Bharat"

---
+++ Imdb.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+
+
+class imdb:
+    def __init__(self, title):
+        self.url = 'http://www.omdbapi.com'
+        self.title = title
+        self.api_key = 'b1c41fdf'
+        self.values = {}
+
+    def check_error(self):
+        self.payload = {'t': self.title, 'r': 'json', 'apikey': self.api_key}
+        try:
+            self.values = requests.get(self.url, params=self.payload).json()
+            print self.values['Error']
+        except ValueError:
+            print (
+                'Caught ValueError Exception, Please check if URL is correct and Up and running')
+        except KeyError:
+            pass
+        return self.values
+    def parse_data(self):
+        self.movie_data = self.check_error()
+        try:
+            list_of_movies = self.movie_data["Ratings"]
+            print 'Movie Name ==>'+str(sys.argv[1])
+            test = filter(lambda movie: movie['Source'] == 'Rotten Tomatoes', list_of_movies)
+            if test:
+                print "Rotten Tomatoes Ratings -->" + \
+                    map(str, [dic['Value'] for dic in test])[0]
+            else:
+                print "Rotten Tomatoes Ratings not present, Displaying others rating \n"
+                print map(str, [dic['Source'] for dic in list_of_movies])[0], map(str, [dic['Value'] for dic in list_of_movies])[0]
+        except KeyError:
+            pass
+
+   
+if len(sys.argv) == 1:
+    raise Exception('Please provide movie name as an argument')
+else:
+    movie = imdb(sys.argv[1])
+    movie.parse_data()
